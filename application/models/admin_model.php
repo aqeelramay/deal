077835e498794fe639a7d73073ce1d44b91e1194
aqeelramay <aqeelramay@gmail.com>
2017-12-16 22:09:30 +0500

    Basic Files

---
+++ application/models/admin_model.php
@@ -0,0 +1,45 @@
+<?php
+
+class Admin_model extends CI_Model {
+	
+	function can_login()
+	{
+		$this->db->where('username',mysql_real_escape_string($this->input->post('username')));
+		$this->db->where('password',md5(mysql_real_escape_string($this->input->post('password'))));
+		$query= $this->db->get('adminuser');
+		
+		if($query->num_rows() == 1)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+	
+	function change_password($password)
+	{
+		$this->db->set('password', md5($password));
+		$this->db->update('adminuser');
+	}
+	
+	function admin_profile()
+	{
+		$query = $this->db->query('SELECT * FROM adminuser');
+		if($query->num_rows() > 0)
+		{
+			foreach($query->result() as $row)
+			{
+				$data[] = $row;	
+			}
+			
+			return $data;	
+		}
+	}
+	
+	function update_profile($update_profile)
+	{
+		$this->db->update('admin_setting', $update_profile);
+	}				
+}
---
+++ application/views/ramay/login.php
@@ -0,0 +1,3 @@
+<?php include("backend/include/header.php"); ?>
+<h1>Hello, world!</h1>
+<?php include("backend/include/footer.php"); ?>
